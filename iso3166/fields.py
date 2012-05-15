57ac96100b418ef1cc23b38c5fc0617c37323948
Valentin v. Seggern <valentin@crowflying.com>
2012-05-15 18:30:27 +0200

    Removed database dependent constraints from field type

--- iso3166/fields.py
+++ iso3166/fields.py
@@ -18,23 +18,6 @@ class ISONumericField(PositiveSmallIntegerField):
     def get_internal_type(self):
         return "PositiveSmallIntegerField"
 
-    def db_type(self):
-        from django.conf import settings
-        if settings.DATABASE_ENGINE in ('postgresql', 'postgresql_psycopg2'):
-            format = 'smallint CHECK ("%(column)s" >= 0 and ' + \
-                   '"%(column)s" < 1000)'
-        elif settings.DATABASE_ENGINE == 'oracle':
-            format = 'NUMBER(11) CHECK (%(qn_column)s >= 0 and ' + \
-                   '%(qn_column)s < 1000)'
-        elif settings.DATABASE_ENGINE == 'sqlite3':
-            format = 'smallint unsigned'
-        elif settings.DATABASE_ENGINE == 'mysql':
-            format = 'smallint UNSIGNED'
-        else:
-            raise UnsupportedBackend()
-        return format % \
-               DictWrapper(self.__dict__, connection.ops.quote_name, "qn_")
-
     def formfield(self, **kwargs):
         """
         Set maximum value in *formfield* to 999.
